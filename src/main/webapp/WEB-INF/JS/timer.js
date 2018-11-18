60891d119b46cf3802b686357b7fb5669aa78dc1
Piotr Pindel <heronavan@gmail.com>
2018-11-18 21:55:12 +0100

    added front - html, css and js

---
+++ src/main/webapp/WEB-INF/JS/timer.js
@@ -0,0 +1,83 @@
+function odliczanie()
+{
+    var dzisiaj = new Date();
+    
+    var dzien = dzisiaj.getDate();
+    var miesiac = dzisiaj.getMonth()+1;
+    var rok = dzisiaj.getFullYear();
+
+    var godzina = dzisiaj.getHours();
+    if (godzina<10)godzina="0"+godzina;
+    var minuta = dzisiaj.getMinutes();
+    if (minuta<10) minuta="0"+minuta;
+    var sekunda = dzisiaj.getSeconds();
+    if (sekunda<10) sekunda="0"+sekunda;
+
+     document.getElementById("zegar").innerHTML = 
+    dzien+"/"+miesiac+"/"+rok+" | "+godzina+":"+minuta+":"+sekunda;
+
+    setTimeout("odliczanie()",1000);
+
+}
+
+
+var numer1 = Math.floor(Math.random()*5)+1;
+var numer2 = Math.floor(Math.random()*4)+1;
+var timer1 = 0;
+var timer2 = 0;
+var timer3 = 0;
+var timer4 = 0;
+
+
+function schowaj1()
+        {
+            $("#pilkarz").fadeOut(500);
+        }
+function schowaj2()
+        {
+            $("#team").fadeOut(500);
+        }
+function zmienslajd1()
+        {
+            numer1++; 
+            if(numer1>5) numer1=1;
+           
+            var plik = "<img src=\"../resources/player" + numer1 +".jpg\"  width=230px height=300px  />";
+
+            document.getElementById("pilkarz").innerHTML = plik;
+            $("#pilkarz").fadeIn(500)
+
+          
+
+            timer1 = setTimeout("zmienslajd1()", 5000);
+            timer2 = setTimeout("schowaj1()", 4500);
+
+        }
+
+function zmienslajd2()
+        {
+            numer2++; 
+            if(numer2>5) numer2=1;
+
+            var plik = "<img src=\"../resources/team" + numer2 +".jpg\" width=230px height=300px />";
+
+            document.getElementById("team").innerHTML = plik;
+            $("#team").fadeIn(500)
+
+           
+
+            timer3 = setTimeout("zmienslajd2()", 5000);
+            timer4 = setTimeout("schowaj2()", 4500);
+
+        }
+
+
+    function start()
+    {
+        odliczanie();
+        zmienslajd1();
+        setTimeout("schowaj2()",2000);
+        setTimeout("zmienslajd2()",2500);
+        
+
+    }
